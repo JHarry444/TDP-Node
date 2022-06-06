e3d6511acd4ea6b221786f9215fd934f342a780f
jordan <jharrison1@qa.com>
2022-06-06 15:18:56 +0100

    finished example

--- index.js
+++ index.js
@@ -6,4 +6,15 @@ const me = {
     job: "Trainer"
 }
 
-console.log("Me: ", me)
+console.log("Me: ", me);
+
+const axios = require("axios"); // imports the axios library
+
+axios.get("https://pokeapi.co/api/v2/pokemon/sneasel")
+    .then(res => {
+        console.log("My favourite pokemon is", res.data.name);
+    }).catch(err => console.error(err));
+
+const maths = require("./maths");
+
+console.log(maths.add(1, 1));
